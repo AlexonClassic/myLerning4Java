5c046c5780fbe089e71d61beec127671d70513c7
Olexandr Shapowal <alexonclassic@ukr.net>
2016-09-23 15:45:08 +0300

    Урок 5. Задание 2: Написать функцию, которая принимает массив чисел в качестве аргумента, увеличивает его первые 3 элемента на 1 и возвращает их сумму как результат. После изменения массив и сумму надо вывести на экран

---
+++ src/ProgKiev/JavaStart/Lesson_5_Task_2.java
@@ -0,0 +1,27 @@
+package ProgKiev.JavaStart;
+
+import java.util.Arrays;
+
+/**
+ * Created by Олександр Шаповал on 13.06.2016.
+ *
+ * Написать функцию, которая принимает массив чисел в качестве аргумента,
+ * увеличивает его первые 3 элемента на 1 и возвращает их сумму как результат.
+ * После изменения массив и сумму надо вывести на экран
+ */
+
+public class Lesson_5_Task_2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int summa = 0;
+
+        //arraySumma(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Сумма первых трех чисел: " + summa);
+    }
+
+    static void arraySumma(int summa, int... n) {
+
+        return;
+    }
+}
