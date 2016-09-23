4e0f1b20c73062da02008846d18afe1d0e544ff2
Olexandr Shapowal <alexonclassic@ukr.net>
2016-09-23 15:06:03 +0300

    Java Start - Bohdan Package Example - 1

---
+++ src/ProgKiev/JavaStart/package_sample/PackageSample.java
@@ -0,0 +1,12 @@
+package ProgKiev.JavaStart.package_sample;
+
+/**
+ * @author Bohdan Vanchuhov
+ */
+
+public class PackageSample {
+    public static void main(String[] args) {
+        ProgKiev.JavaStart.package_sample.my_package.MyClass myObject =
+                new ProgKiev.JavaStart.package_sample.my_package.MyClass();
+    }
+}
