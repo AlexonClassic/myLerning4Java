67c55d014dd10c93d0e5caf92c20953284f6cefc
Olexandr Shapowal <alexonclassic@ukr.net>
2016-09-23 15:49:59 +0300

    Java Start - Bohdan - Array Example - 6 (не окончен): Две звезды: Насать метод который заполняет массив: 0, 0, 1, 1, 2, 2, 3, 3, ... до конца

---
+++ src/ProgKiev/JavaStart_Bohdan/Arrayys/ArrSample6.java
@@ -0,0 +1,20 @@
+package ProgKiev.JavaStart_Bohdan.Arrayys;
+
+import java.util.Random;
+
+/**
+ * Created by Олександр Шаповал on 11.07.2016.
+ *
+ * Две звезды:
+ * Насать метод который заполняет массив:
+ * 0, 0, 1, 1, 2, 2, 3, 3, ... до конца
+ *
+ * a[i] = i / 2;
+ */
+
+public class ArrSample6 {
+    public static void main(String[] args) {
+        Random rnd = new Random(100);
+        int[] array = new int[10];
+    }
+}
