d783e4dae363a1acab023160699d398781f37791
Olexandr Shapowal <alexonclassic@ukr.net>
2016-09-23 14:54:00 +0300

    Видео урок №10

---
+++ src/VideoLesons/Scanner_Input.java
@@ -0,0 +1,49 @@
+package VideoLesons;
+
+import java.util.Scanner;
+
+/**
+ * Created by Олександр Шаповал on 18.05.2016.
+ */
+
+public class Scanner_Input {
+    public static void main(String[] args) {
+        /* String s1, s2, s3, x;
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите что-нибудь");
+        x = s.nextLine();
+        System.out.println("Вы ввели: " + x);
+
+        /* ------------------------------------ */
+
+        /* Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первую сторку");
+        s1 = scanner.nextLine();
+        System.out.println("Вы ввели:" + s1);
+        System.out.println("Введите вторую сторку");
+        s2 = scanner.nextLine();
+        System.out.println("Вы ввели:" + s2);
+        System.out.println("Введите третью сторку");
+        s3 = scanner.nextLine();
+        System.out.println("Вы ввели:" + s3);
+
+        System.out.println(s1 + " " + s2);
+        System.out.println(s3 + " " + s2 + " " + s1);
+        System.out.println(s1 + " " + s2 + " " + s2);
+
+        /* ------------------------------------ */
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int x1 = s.nextInt();
+        System.out.print("Введите второе число: ");
+        int x2 = s.nextInt();
+
+        System.out.println("\nРезультат:");
+        System.out.println(x1 + x2);
+        System.out.println(x1 * x2);
+        System.out.println(x1 / x2);
+        System.out.println(x1 % x2);
+    }
+}
