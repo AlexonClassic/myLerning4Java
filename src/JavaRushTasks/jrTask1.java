f0ebf8b1c586878a0452526941f6750270c8c2be
Olexandr Shapowal <alexonclassic@ukr.net>
2016-09-23 13:53:01 +0300

    JavaRush - Task - 1

---
+++ src/JavaRushTasks/jrTask1.java
@@ -0,0 +1,39 @@
+package JavaRushTasks;
+
+/**
+ * Created by Олександр Шаповал on 08.06.2016.
+ *
+ * JavaRush - Task - 1
+ */
+
+public class jrTask1 {
+    public static void main(String[] args) {
+    /*     String a = "JavaRush. Learn once - use anywhere";
+        int x = 10;
+        print5Times(a, x);
+    }
+
+    public static void print5Times(String a, int x) {
+        for (int i = 0; i < x; i++) {
+            System.out.println(a);
+        }
+    }
+
+    Должна компилироваться
+    Внесите изменения в программу, чтобы переменная
+    s была равна 5,
+    t была равна 6,
+    x равна 7.
+
+    Программа должна компилироваться.
+
+
+Nothing personal, it's just business.
+
+
+    public class Solution {*/
+        double s = Math.pow(5, 4);
+
+        System.out.println(s);
+    }
+}
