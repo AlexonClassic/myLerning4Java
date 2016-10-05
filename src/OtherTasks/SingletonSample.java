ee90095f6061391d3f589cabfa3100a776a0090b
Olexandr Shapowal <alexonclassic@ukr.net>
2016-10-05 13:14:03 +0300

    Singleton Sample

---
+++ src/OtherTasks/SingletonSample.java
@@ -0,0 +1,22 @@
+package OtherTasks;
+
+/**
+ * @author Bohdan Vanchuhov
+ */
+
+public class SingletonSample {
+    public static void main(String[] args) {
+        Elvis elvis = Elvis.INSTANCE;
+
+        System.out.println(elvis);
+    }
+}
+
+enum Elvis {
+    INSTANCE;
+
+    @Override
+    public String toString() {
+        return "I'm king";
+    }
+}
