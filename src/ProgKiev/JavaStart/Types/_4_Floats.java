628b6f681dba92754c68e215b8738fe760200be4
Olexandr Shapowal <alexonclassic@ukr.net>
2016-10-26 02:48:53 +0300

    Floats example

---
+++ src/ProgKiev/JavaStart/Types/_4_Floats.java
@@ -0,0 +1,27 @@
+package ProgKiev.JavaStart.Types;
+
+/**
+ * @author Bohdan Vanchuhov
+ */
+
+public class _4_Floats {
+    public static void main(String[] args) {
+        double d1 = 1.1;
+        double d2 = 1.;
+        double d3 = .1;
+        double d4 = 1234567890987654567898765432345678909876544323456789.;
+        double d5 = 1.1E-1;
+        double d6 = 1000;
+
+        //--------------------------------------------------
+
+//        float f1 = 1.1;
+        float f2 = 1.1f;
+        float f3 = 1000;
+
+        long l = 1234567890123456789L;
+        float f = l;
+        System.out.println(f);
+        System.out.println((long)f);
+    }
+}
