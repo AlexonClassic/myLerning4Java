3e82d5211374a73c51a7ea1cf9ee054c95e44021
Olexandr Shapowal <alexonclassic@ukr.net>
2016-10-26 02:49:20 +0300

    Float Operations example

---
+++ src/ProgKiev/JavaStart/Types/_5_FloatOperations.java
@@ -0,0 +1,28 @@
+package ProgKiev.JavaStart.Types;
+
+/**
+ * @author Bohdan Vanchuhov
+ */
+
+public class _5_FloatOperations {
+    public static void main(String[] args) {
+        double x = 10.;
+        double y = 3.;
+
+        System.out.println("x + y = " + (x + y));
+        System.out.println("x - y = " + (x - y));
+        System.out.println("x * y = " + (x * y));
+        System.out.println("x / y = " + (x / y));
+        System.out.println("x % y = " + (x % y));
+        System.out.println();
+
+        System.out.println("x++ = " + (x++));
+        System.out.println("x = " + x);
+        System.out.println("++x = " + (++x));
+        System.out.println();
+
+        System.out.println("y-- = " + (y--));
+        System.out.println("y = " + y);
+        System.out.println("--y = " + (--y));
+    }
+}
