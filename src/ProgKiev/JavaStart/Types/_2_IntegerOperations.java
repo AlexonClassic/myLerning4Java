3121b8ac4074c06ba72baa50d97d5410837f1396
Olexandr Shapowal <alexonclassic@ukr.net>
2016-10-26 02:47:44 +0300

    Integer Operations example

---
+++ src/ProgKiev/JavaStart/Types/_2_IntegerOperations.java
@@ -0,0 +1,32 @@
+package ProgKiev.JavaStart.Types;
+
+/**
+ * @author Bohdan Vanchuhov
+ */
+
+public class _2_IntegerOperations {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 0;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println();
+
+        System.out.println("x + y = " + (x + y));
+        System.out.println("x - y = " + (x - y));
+        System.out.println("x * y = " + (x * y));
+        System.out.println("x / y = " + (x / y));
+        System.out.println("x % y = " + (x % y));
+        System.out.println();
+
+        System.out.println("x++ = " + (x++));
+        System.out.println("x = " + x);
+        System.out.println("++x = " + (++x));
+        System.out.println();
+
+        System.out.println("y-- = " + (y--));
+        System.out.println("y = " + y);
+        System.out.println("--y = " + (--y));
+    }
+}
