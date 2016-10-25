45b89c4e9aa4c3f834e1faec7ab04960d9f569ae
Olexandr Shapowal <alexonclassic@ukr.net>
2016-10-26 02:50:37 +0300

    String example

---
+++ src/ProgKiev/JavaStart/Types/_8_String.java
@@ -0,0 +1,17 @@
+package ProgKiev.JavaStart.Types;
+
+/**
+ * @author Bohdan Vanchuhov
+ */
+
+public class _8_String {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "world";
+        String message = s1 + ", " + s2;
+
+        System.out.println(message);
+
+        System.out.println(1 + 1 + "Hello" + 1 + 1);
+    }
+}
