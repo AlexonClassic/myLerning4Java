a0baf941cf36479ec597e2bf100b36750152e77b
Olexandr Shapowal <alexonclassic@ukr.net>
2016-10-26 02:53:18 +0300

    Division by zero example

---
+++ src/ProgKiev/JavaStart/Types/DivByZero.java
@@ -0,0 +1,12 @@
+package ProgKiev.JavaStart.Types;
+
+/**
+ * @author Bohdan Vanchuhov
+ */
+
+public class DivByZero {
+    public static void main(String[] args) {
+        System.out.println(1 / 0);
+        System.out.println(1. / 0);
+    }
+}
