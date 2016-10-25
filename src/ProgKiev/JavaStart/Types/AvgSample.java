9416730a3c2a5fae6195662437dd9c0fe2f4b9f5
Olexandr Shapowal <alexonclassic@ukr.net>
2016-10-26 02:45:47 +0300

    Finde averege digit

---
+++ src/ProgKiev/JavaStart/Types/AvgSample.java
@@ -0,0 +1,17 @@
+package ProgKiev.JavaStart.Types;
+
+/**
+ * @author Bohdan Vanchuhov
+ *
+ * Finde averege digit
+ */
+
+public class AvgSample {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+
+        double avg = (a + b) / 2.;
+        System.out.println(avg);
+    }
+}
