9f3f9d85fd8e19cdc8d30d2547556a9cfebc9861
Olexandr Shapowal <alexonclassic@ukr.net>
2016-10-26 02:53:54 +0300

    Double Comparison example

---
+++ src/ProgKiev/JavaStart/Types/DoubleComparison.java
@@ -0,0 +1,17 @@
+package ProgKiev.JavaStart.Types;
+
+/**
+ * @author Bohdan Vanchuhov
+ */
+
+public class DoubleComparison {
+    public static void main(String[] args) {
+        double d1 = 0.3;
+        double d2 = 0.1 + 0.2;
+
+        System.out.println(d1 == d2);
+
+        System.out.println(d1);
+        System.out.println(d2);
+    }
+}
