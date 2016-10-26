57f994641f29b94064798c53b3c55cc9b2e9c4ec
Olexandr Shapowal <alexonclassic@ukr.net>
2016-10-26 16:32:44 +0300

    Мой курсовой проект по Java OOP

--- src/ProgKiev/JavaOOP/MyCourseProject/Utils/Errors.java
+++ src/ProgKiev/JavaOOP/MyCourseProject/Utils/Errors.java
@@ -13,4 +13,11 @@ public class Errors {
             System.out.print("Yor choice must by from 1 to 2. You enter - " + choiceTypeRegistration + ". It's wrong number! Please, re-enter your choice: ");
         }
     }
+
+    public void WrongChoiceJob(byte choiceJob) {
+        if (choiceJob > 6) {
+            System.out.println("----------------------");
+            System.out.print("Yor choice must by from 1 to 6. You enter - " + choiceJob + ". It's wrong number! Please, re-enter your choice: ");
+        }
+    }
 }
