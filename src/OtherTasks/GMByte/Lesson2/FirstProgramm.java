8928aefe862cb04a5d551191c0bdb321434160d6
Olexandr Shapowal <alexonclassic@ukr.net>
2016-11-11 22:16:22 +0200

    Lesson - 2

---
+++ src/OtherTasks/GMByte/Lesson2/FirstProgramm.java
@@ -0,0 +1,24 @@
+package OtherTasks.GMByte.Lesson2;
+
+/**
+ * Created by Олександр Шаповал on 11.11.2016.
+ *
+ * Lesson - 2
+ * https://youtu.be/aWf2uCcC2Nk?list=PLk95cAdGYXN1GYSm1NIMHJuyq_kk9Obg9
+ */
+
+public class FirstProgramm {
+    public static void main(String[] args) {
+        System.out.println("Ура! Сделан мой первый шаг в java!");
+        System.out.println("Ура! Сделан мой первый шаг в java!");
+        System.out.println("Ура! Сделан мой первый шаг в java!");
+        System.out.println("Ура! Сделан мой первый шаг в java!");
+        System.out.println("Ура! Сделан мой первый шаг в java!");
+        System.out.println();
+        System.out.print("Ура! Сделан мой первый шаг в java! ");
+        System.out.print("Ура! Сделан мой первый шаг в java! ");
+        System.out.print("Ура! Сделан мой первый шаг в java! ");
+        System.out.print("Ура! Сделан мой первый шаг в java! ");
+        System.out.print("Ура! Сделан мой первый шаг в java! ");
+    }
+}
