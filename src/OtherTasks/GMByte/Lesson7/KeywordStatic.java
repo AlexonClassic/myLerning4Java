511020df2296b5c63a66eabdfeb5275d70ab45d3
Olexandr Shapowal <alexonclassic@ukr.net>
2016-11-11 23:37:23 +0200

    Keyword Static

---
+++ src/OtherTasks/GMByte/Lesson7/KeywordStatic.java
@@ -0,0 +1,16 @@
+package OtherTasks.GMByte.Lesson7;
+
+/**
+ * Created by Олександр Шаповал on 11.11.2016
+ *
+ * Lesson - 7
+ * https://youtu.be/tJ8N4pafRl8?list=PLk95cAdGYXN1GYSm1NIMHJuyq_kk9Obg9
+ *
+ * https://docviewer.yandex.ua/?url=ya-disk-public%3A%2F%2FWRJzA1WIPQGSpsK1qOgpikP07Sl4LwzEVcat7oIksDQ%3D&name=Domashnie_zadania_k_urokam_1-_15.txt&page=2&c=5826337a13af
+ */
+
+public class KeywordStatic {
+    public static void main(String[] args) {
+        KeywordStaticRunner.numberInVariable();
+    }
+}
