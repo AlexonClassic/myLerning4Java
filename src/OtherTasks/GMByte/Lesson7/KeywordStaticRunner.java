d5eb5bf3fe12cfa5b4e72608ece4d721f04ad670
Olexandr Shapowal <alexonclassic@ukr.net>
2016-11-14 15:05:54 +0200

    Keyword Static

--- src/OtherTasks/GMByte/Lesson7/KeywordStaticRunner.java
+++ src/OtherTasks/GMByte/Lesson7/KeywordStaticRunner.java
@@ -4,7 +4,12 @@ package OtherTasks.GMByte.Lesson7;
  * Created by Олександр Шаповал on 11.11.2016
  *
  * Lesson - 7
+ *
  * https://youtu.be/tJ8N4pafRl8?list=PLk95cAdGYXN1GYSm1NIMHJuyq_kk9Obg9
+ *
+ * 1) Создайте переменную типа byte и статический метод. Метод должен
+ * выводить на экран текст: "В переменной (имя Вашей переменной)
+ * хранится число (значение Вашей переменной)".
  *
  * https://docviewer.yandex.ua/?url=ya-disk-public%3A%2F%2FWRJzA1WIPQGSpsK1qOgpikP07Sl4LwzEVcat7oIksDQ%3D&name=Domashnie_zadania_k_urokam_1-_15.txt&page=2&c=5826337a13af
  */
