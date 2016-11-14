1c156fe5bb133a8bb74ce3c1e01ab650a4f1cc29
Olexandr Shapowal <alexonclassic@ukr.net>
2016-11-14 15:28:23 +0200

    Constructors

---
+++ src/OtherTasks/GMByte/Lesson10/Constructors.java
@@ -0,0 +1,43 @@
+package OtherTasks.GMByte.Lesson10;
+
+/**
+ * Created by Олександр Шаповал on 11.11.2016
+ *
+ * Lesson - 10
+ *
+ * https://youtu.be/WAeT1eW4mr4?list=PLk95cAdGYXN1GYSm1NIMHJuyq_kk9Obg9
+ *
+ * 1) Создайте класс. В нём создайте четыре переменных.
+ * При создании объекта все переменные должны инициализироватся
+ * (получать значения) и на экран должен быть выведен текст:
+ * "Меня создали!".
+ *
+ * 2) Подумайте, можно ли с помощью конструктора инициализировать
+ * статическую переменную?
+ *
+ * https://docviewer.yandex.ua/?url=ya-disk-public%3A%2F%2FWRJzA1WIPQGSpsK1qOgpikP07Sl4LwzEVcat7oIksDQ%3D&name=Domashnie_zadania_k_urokam_1-_15.txt&page=2&c=5826337a13af
+ */
+
+public class Constructors {
+    byte a;
+    byte b;
+    byte c;
+    byte d;
+
+    public Constructors(byte a, byte b, byte c, byte d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+
+        System.out.println("---------------------------");
+        System.out.println();
+        System.out.println("Variable \"byte a\", contains argument: " + a + ". I created");
+        System.out.println("---------------------------");
+        System.out.println("Variable \"byte b\", contains argument: " + b + ". I created");
+        System.out.println("---------------------------");
+        System.out.println("Variable \"byte c\", contains argument: " + c + ". I created");
+        System.out.println("---------------------------");
+        System.out.println("Variable \"byte d\", contains argument: " + d + ". I created");
+    }
+}
---
+++ src/OtherTasks/GMByte/Lesson10/ConstructorsRunner.java
@@ -0,0 +1,25 @@
+package OtherTasks.GMByte.Lesson10;
+
+/**
+ * Created by Олександр Шаповал on 11.11.2016
+ *
+ * Lesson - 10
+ *
+ * https://youtu.be/WAeT1eW4mr4?list=PLk95cAdGYXN1GYSm1NIMHJuyq_kk9Obg9
+ *
+ * 1) Создайте класс. В нём создайте четыре переменных.
+ * При создании объекта все переменные должны инициализироватся
+ * (получать значения) и на экран должен быть выведен текст:
+ * "Меня создали!".
+ *
+ * 2) Подумайте, можно ли с помощью конструктора инициализировать
+ * статическую переменную?
+ *
+ * https://docviewer.yandex.ua/?url=ya-disk-public%3A%2F%2FWRJzA1WIPQGSpsK1qOgpikP07Sl4LwzEVcat7oIksDQ%3D&name=Domashnie_zadania_k_urokam_1-_15.txt&page=2&c=5826337a13af
+ */
+
+public class ConstructorsRunner {
+    public static void main(String[] args) {
+        Constructors constructors = new Constructors((byte)5, (byte)7, (byte)10, (byte)-100);
+    }
+}
