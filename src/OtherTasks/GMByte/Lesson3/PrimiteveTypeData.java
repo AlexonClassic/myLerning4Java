ccaedc9d16cf86cf0c0b2b040473dd7b7a3f739b
Olexandr Shapowal <alexonclassic@ukr.net>
2016-11-14 14:59:14 +0200

    Primiteve Type Data

--- src/OtherTasks/GMByte/Lesson3/PrimiteveTypeData.java
+++ src/OtherTasks/GMByte/Lesson3/PrimiteveTypeData.java
@@ -5,7 +5,10 @@ package OtherTasks.GMByte.Lesson3;
  *
  * Lesson - 3
  *
- * 1) Создайте две переменные типа byte и запишите в них любые числа (помните про диапазон от -128 до +127).
+ * https://youtu.be/x82BOGrO310?list=PLk95cAdGYXN1GYSm1NIMHJuyq_kk9Obg9
+ *
+ * 1) Создайте две переменные типа byte и запишите в них любые числа
+ * (помните про диапазон от -128 до +127).
  *
  * 1.1) Выведите на экран строку: "Первая переменная хранит значение
  * (значение Вашей первой переменной)" и ещё одну строку "Вторая
@@ -17,7 +20,7 @@ package OtherTasks.GMByte.Lesson3;
  * Дополнительно: Выполните с этими переменными все четыре операции(+,
  * - , \ , *) и каждый раз выводите результат на экран.
  *
- * https://youtu.be/x82BOGrO310?list=PLk95cAdGYXN1GYSm1NIMHJuyq_kk9Obg9
+ * https://docviewer.yandex.ua/?url=ya-disk-public%3A%2F%2FWRJzA1WIPQGSpsK1qOgpikP07Sl4LwzEVcat7oIksDQ%3D&name=Domashnie_zadania_k_urokam_1-_15.txt&page=1&c=5829b3df2c26
  */
 
 public class PrimiteveTypeData {
