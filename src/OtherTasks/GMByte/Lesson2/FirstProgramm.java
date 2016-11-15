c6abee6e5d21d92251d46e576eea348885dfd168
Olexandr Shapowal <alexonclassic@ukr.net>
2016-11-15 20:55:55 +0200

    First Programm

--- src/OtherTasks/GMByte/Lesson2/FirstProgramm.java
+++ src/OtherTasks/GMByte/Lesson2/FirstProgramm.java
@@ -3,7 +3,7 @@ package OtherTasks.GMByte.Lesson2;
 /**
  * Created by Олександр Шаповал on 11.11.2016
  *
- * Lesson - 2
+ * Lesson - 2 - Первая программа:
  *
  * https://youtu.be/aWf2uCcC2Nk?list=PLk95cAdGYXN1GYSm1NIMHJuyq_kk9Obg9
  *
